0cfa53a481a73e59db0fbe5b00e6906a26ef9339
malikjanattar <malikjanattar7866@gmail.com>
2023-12-30 17:21:07 +0530

    created base model, model training and model evaluation pipeline with mlflow integration

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # KidneyClassifier-Mlops-Project
+
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/malikjanattar/KidneyClassifier-Mlops-Project.mlflow \
+MLFLOW_TRACKING_USERNAME=malikjanattar \
+MLFLOW_TRACKING_PASSWORD=fb235184d93530b182445af40faa24f200449a3c \
+python script.py
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from src.KidneyClassifier import logger
 #logger.info("welcome to our project")
 from KidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
-
-
+from KidneyClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from KidneyClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from KidneyClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
@@ -15,6 +15,44 @@ try:
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
