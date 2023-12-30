2262325d2e05e47e054565e4f7fc028a7f171522
malikjanattar <malikjanattar7866@gmail.com>
2023-12-30 12:52:42 +0530

    created data ingestion pipeline

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from src.KidneyClassifier import logger
-logger.info("welcome to our project")
+#logger.info("welcome to our project")
+from KidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+
+
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
